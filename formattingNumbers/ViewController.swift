2ffeb18bc52f291652dbd990fe16bd29146d1e31
Alex <alexcc17@gmail.com>
2019-04-11 19:11:24 -0400

    Updated.

--- formattingNumbers/ViewController.swift
+++ formattingNumbers/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var unformattedNumLbl: UILabel!
+    @IBOutlet weak var formattedNumLbl: UILabel!
+    @IBAction func formatNumBtnPressed(_ sender: UIButton) {
+        formatNum(number: unformattedNumLbl.text!)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        unformattedNumLbl.text = "  (213)-866-5785    "
+    }
+    
+    func formatNum(number: String) {
+        let formNumber = String(number.filter { !" -()".contains($0) })
+        var newNumber: String?
+        if formNumber.hasPrefix("+"){
+            newNumber = formNumber
+        } else {
+            newNumber = "+1\(formNumber)"
+        }
+        formattedNumLbl.text = newNumber
     }
 
 
